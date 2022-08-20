775b3d90a343bbc8eb1bff6b3dfd506d9445ada0
mynameisjeff99 <jeff.guoshengc@gmail.com>
2022-08-20 13:02:08 -0400

    Add delete GET and POST

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -161,11 +161,62 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = (req, res, next) => {    
+  Genre.findById(req.params.id, (err, genre) => {
+    if (err) {
+      return next(err);
+    }
+    if (genre==null) {
+      // No results.
+      var err = new Error('Genre not found');
+      err.status = 404;
+      return next(err);
+    }
+  // Success.
+  res.render('genre_form', { title: 'Update Genre', genre: genre});
+  });
 };
 
+
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  // validate and sanitize the name field.
+  body("name", "Genre name required")
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id});
+    if (!errors.isEmpty()) {
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({name: req.body.name}).exec((err, found_genre) => {
+        if (err) {
+          return next(err);
+        }
+        if (found_genre) {
+          res.redirect(found_genre.url);
+        } else {
+          // Success
+          Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(thegenre.url);
+          });
+        }
+      });
+    }
+  },
+];
